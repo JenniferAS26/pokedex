68063de7efcc27bf34a589151ce3b754467addaf
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-08-13 18:02:12 -0500

    Cleaning code

---
+++ scripts/api.js
@@ -0,0 +1,19 @@
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
+
+const fetchAll = async () => {
+    const response = await axios.get(BASE_URL) 
+    const results = response.data.results
+    let details = []
+    for(const result of results){        
+        let detailsResponse = await axios.get(result.url)
+        details.push(detailsResponse.data)
+    }
+    return details
+}
+
+const fetchById = async id => {
+    const response = await axios.get(`${BASE_URL}${id}`);
+    return response.data;
+}
+
+export { fetchAll, fetchById }
